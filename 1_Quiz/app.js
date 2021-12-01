045e1983bea7b7715d9420929eae780051be84e0
Naomi Mango <naomi.mango@gmail.com>
2021-12-01 14:29:06 +0100

    [MAJ] P1 Quiz JS - Obtenir les résultats

--- 1_Quiz/app.js
+++ 1_Quiz/app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector(".form-quiz");
 let tableauResultats = [];
+const reponses = ["c", "a", "b", "a", "c"];
+const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+const titreResultat = document.querySelector(".resultats h2");
+const texteResultat = document.querySelector(".note");
+const aideResultat = document.querySelector(".aide");
+const toutesLesQuestions = document.querySelectorAll(".question-block");
+let verifTableau = [];
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,6 +16,19 @@ form.addEventListener("submit", (e) => {
             document.querySelector(`input[name="q${i}"]:checked`).value
         );
     }
-    console.log(tableauResultats);
+    // console.log(tableauResultats);
+    verifFunc(tableauResultats);
     tableauResultats = [];
 });
+
+function verifFunc(tabResultats) {
+    for (let a = 0; a < 5; a++) {
+        if (tabResultats[a] === reponses[a]) {
+            verifTableau.push(true);
+        } else {
+            verifTableau.push(false);
+        }
+    }
+    console.log(verifTableau);
+    verifTableau = [];
+}
