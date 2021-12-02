a6ebe3082d7506b1e93efa6514478528e70f5e37
Naomi Mango <naomi.mango@gmail.com>
2021-12-02 06:10:12 +0100

    [MAJ] P4 Cookies JS - Remplir l'objet contenant les infos des cookies

--- 4_Cookies/app.js
+++ 4_Cookies/app.js
@@ -1,4 +1,8 @@
 const inputDate = document.querySelector("input[type=date]");
+const affichage = document.querySelector(".affichage");
+const btns = document.querySelectorAll("button");
+const inputs = document.querySelectorAll("input");
+const infoTxt = document.querySelector(".info-txt");
 
 const today = new Date();
 const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000); // additionner une semaine en ms
@@ -9,3 +13,31 @@ let month = ("" + (nextWeek.getMonth() + 1)).slice(-3);
 // console.log(month);
 let year = today.getFullYear();
 inputDate.value = `${year}-${month}-${day}`;
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", btnAction);
+});
+
+function btnAction(e) {
+    let nvObj = {};
+
+    inputs.forEach((input) => {
+        let attrName = input.getAttribute("name");
+        let attrValeur =
+            attrName !== "cookieExpire" ? input.value : input.valueAsDate;
+        nvObj[attrName] = attrValeur;
+    });
+    // console.log(nvObj);
+
+    let description = e.target.getAttribute("data-cookie");
+
+    if (description === "creer") {
+        creerCookie(nvObj.cookieName, nvObj.cookieValue, nvObj.cookieExpire);
+    } else if (description === "toutAfficher") {
+        listeCookies();
+    }
+}
+
+function creerCookie(name, value, exp) {}
+
+function listeCookies() {}
