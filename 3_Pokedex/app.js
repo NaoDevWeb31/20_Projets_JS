babb976260743fb2ab6b49b6474e44368a252902
Naomi Mango <naomi.mango@gmail.com>
2021-12-02 03:13:36 +0100

    [MAJ] P3 Pokédex  JS - Gérer les données pour les avoir en français

--- 3_Pokedex/app.js
+++ 3_Pokedex/app.js
@@ -1,13 +1,44 @@
+let allPokemon = [];
+let tableauFin = [];
 const searchInput = document.querySelector(".recherche-poke input");
 
 function fetchPokemonBase() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then((reponse) => reponse.json())
         .then((allPoke) => {
-            console.log(allPoke);
+            // console.log(allPoke);
+            allPoke.results.forEach((pokemon) => {
+                fetchPokemonComplet(pokemon);
+            });
         });
 }
 fetchPokemonBase();
+
+function fetchPokemonComplet(pokemon) {
+    let objPokemonFull = {};
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+        .then((reponse) => reponse.json())
+        .then((pokeData) => {
+            // console.log(pokeData);
+            objPokemonFull.pic = pokeData.sprites.front_default;
+            objPokemonFull.type = pokeData.types[0].type.name;
+
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
+                .then((reponse) => reponse.json())
+                .then((pokeData) => {
+                    // console.log(pokeData);
+                    objPokemonFull.name = pokeData.names[4].name;
+                    allPokemon.push(objPokemonFull);
+
+                    if (allPokemon.length === 151) {
+                        console.log(allPokemon);
+                    }
+                });
+        });
+}
 
 // Animation Input
 
