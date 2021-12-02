89021306ea1231e2460567b248107de8e940e135
Naomi Mango <naomi.mango@gmail.com>
2021-12-02 08:24:25 +0100

    [MAJ] P6 Pomodoro JS - Finitions

--- 6_Pomodoro/app.js
+++ 6_Pomodoro/app.js
@@ -5,6 +5,7 @@ const btnPause = document.querySelector(".b2");
 const btnReset = document.querySelector(".b3");
 const cycles = document.querySelector("h2");
 
+let checkInterval = false;
 let tempsInitial = 1800;
 let tempsDeRepos = 300;
 let pause = false;
@@ -19,19 +20,56 @@ affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
 }`;
 
 btnGo.addEventListener("click", () => {
-    tempsInitial--;
-    affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-        tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
-    }`;
+    if (checkInterval === false) {
 
-    let timer = setInterval(() => {
-        if (pause === false && tempsInitial > 0) {
-            tempsInitial--;
-            affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
-                tempsInitial % 60 < 10
-                    ? `0${tempsInitial % 60}`
-                    : tempsInitial % 60
+        checkInterval = true;
+
+        tempsInitial--;
+        affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
             }`;
-        }
-    }, 1000);
+
+        let timer = setInterval(() => {
+            if (pause === false && tempsInitial > 0) {
+                tempsInitial--;
+                affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${tempsInitial % 60 < 10
+                        ? `0${tempsInitial % 60}`
+                        : tempsInitial % 60
+                    }`;
+            } else if (pause === false && tempsDeRepos === 0 && tempsInitial === 0) {
+                tempsInitial = 1800;
+                tempsDeRepos = 300;
+                nbDeCycles++;
+                cycles.innerText = `Nombre de cycles ${nbDeCycles}`;
+                affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+                affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+            } else if (pause === false && tempsInitial === 0) {
+                tempsDeRepos--;
+                affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+            }
+        }, 1000);
+
+        // Reset
+        btnReset.addEventListener("click", () => {
+            clearInterval(timer);
+            checkInterval = false;
+            tempsInitial = 1800;
+            tempsDeRepos = 300;
+            affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+        })
+
+    } else {
+        return;
+    }
 });
+
+btnPause.addEventListener("click", () => {
+
+    if(pause === false){
+        btnPause.innerText = "Play";
+    } else if(pause === true){
+        btnPause.innerText = "Pause";
+    }
+    pause = !pause;
+    
+})
