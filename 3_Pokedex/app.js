7f7b4b6ff9d8198d718721ea81aa691546e43d0b
Naomi Mango <naomi.mango@gmail.com>
2021-12-02 03:29:06 +0100

    [MAJ] P3 Pokédex  JS - Créer les cartes

--- 3_Pokedex/app.js
+++ 3_Pokedex/app.js
@@ -1,6 +1,26 @@
 let allPokemon = [];
 let tableauFin = [];
 const searchInput = document.querySelector(".recherche-poke input");
+const listePoke = document.querySelector(".liste-poke");
+
+const types = {
+    grass: "#78c850",
+    ground: "#E2BF65",
+    dragon: "#6F35FC",
+    fire: "#F58271",
+    electric: "#F7D02C",
+    fairy: "#D685AD",
+    poison: "#966DA3",
+    bug: "#B3F594",
+    water: "#6390F0",
+    normal: "#D9D5D8",
+    psychic: "#F95587",
+    flying: "#A98FF3",
+    fighting: "#C25956",
+    rock: "#B6A136",
+    ghost: "#735797",
+    ice: "#96D9D6",
+};
 
 function fetchPokemonBase() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -25,6 +45,7 @@ function fetchPokemonComplet(pokemon) {
             // console.log(pokeData);
             objPokemonFull.pic = pokeData.sprites.front_default;
             objPokemonFull.type = pokeData.types[0].type.name;
+            objPokemonFull.id = pokeData.id;
 
             fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
                 .then((reponse) => reponse.json())
@@ -34,10 +55,41 @@ function fetchPokemonComplet(pokemon) {
                     allPokemon.push(objPokemonFull);
 
                     if (allPokemon.length === 151) {
-                        console.log(allPokemon);
+                        // console.log(allPokemon);
+                        tableauFin = allPokemon
+                            .sort((a, b) => {
+                                return a.id - b.id;
+                            })
+                            .slice(0, 21);
+                        // console.log(tableauFin);
+
+                        createCard(tableauFin);
                     }
                 });
         });
+}
+
+// Création des cartes
+
+function createCard(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const carte = document.createElement("li");
+        let couleur = types[arr[i].type];
+        carte.style.background = couleur;
+        const txtCarte = document.createElement("h4");
+        txtCarte.innerText = arr[i].name;
+        const idCarte = document.createElement("p");
+        idCarte.innerText = `ID# ${arr[i].id}`;
+        const imgCarte = document.createElement("img");
+        imgCarte.src = arr[i].pic;
+        imgCarte.alt = `Représentation de ${arr[i].name}`;
+
+        carte.appendChild(imgCarte);
+        carte.appendChild(txtCarte);
+        carte.appendChild(idCarte);
+
+        listePoke.appendChild(carte);
+    }
 }
 
 // Animation Input
