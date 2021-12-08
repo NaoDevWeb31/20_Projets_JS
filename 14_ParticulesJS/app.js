2f9872fe0956d551aba8dc749b8e87bec9fc52dd
Naomi Mango <naomi.mango@gmail.com>
2021-12-08 15:48:13 +0100

    [MAJ] P14 Particules - Créer un tableau d'instances de Particule

--- 14_ParticulesJS/app.js
+++ 14_ParticulesJS/app.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+let particuleTab;
 
 class Particule {
     constructor(x, y, directionX, directionY, taille, couleur) {
@@ -19,6 +20,23 @@ class Particule {
         ctx.fill();
     }
 }
-const obj1 = new Particule(300, 300, 50, 50, 100, "black");
-console.log(obj1);
-obj1.dessine();
+// const obj1 = new Particule(300, 300, 50, 50, 100, "black");
+// console.log(obj1);
+// obj1.dessine();
+
+function init() {
+    particuleTab = [];
+    for (let i = 0; i < 100; i++) {
+        let taille = (Math.random() + 0.01) * 20;
+        let x = Math.random() * (window.innerWidth - taille * 2);
+        let y = Math.random() * (window.innerHeight - taille * 2);
+        let directionX = (Math.random() * 0.4) - 0.2;
+        let directionY = (Math.random() * 0.4) - 0.2;
+        let couleur = "white";
+        particuleTab.push(
+            new Particule(x, y, directionX, directionX, taille, couleur)
+        );
+    }
+}
+init();
+console.log(particuleTab);
