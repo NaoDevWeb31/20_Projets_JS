4406d3de82972b67b471b85ee65ae4482638ac9b
Naomi Mango <naomi.mango@gmail.com>
2021-12-09 14:26:25 +0100

    [MAJ] P15 Space-Invaders - Déplacer les aliens

--- 15_Space Invaders/app.js
+++ 15_Space Invaders/app.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".grille");
 let toutesLesDivs;
 let alienInvaders = [];
 let tireurPosition = 229;
+let direction = 1;
 
 function creationGrilleEtAliens() {
     let indexAttr = 0;
@@ -48,3 +49,58 @@ function creationGrilleEtAliens() {
     toutesLesDivs[tireurPosition].classList.add("tireur");
 }
 creationGrilleEtAliens();
+
+
+function deplacerLeTireur(e) {
+    toutesLesDivs[tireurPosition].classList.remove("tireur");
+    if (e.keyCode === 37) {
+        if (tireurPosition > 220) {
+            tireurPosition -= 1;
+        }
+    }
+    if (e.keyCode === 39) {
+        if (tireurPosition < 239) {
+            tireurPosition += 1;
+        }
+    }
+    toutesLesDivs[tireurPosition].classList.add("tireur");
+}
+document.addEventListener("keydown", deplacerLeTireur);
+
+
+// Bouger les aliens
+let descendreRight = true;
+let descendreLeft = true;
+
+function bougerLesAliens() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        if (toutesLesDivs[alienInvaders[i]].getAttribute("data-right") === "true") {
+            if (descendreRight) {
+                direction = 20;
+                setTimeout(() => { descendreRight = false; }, 50);
+            } else if (descendreRight === false) {
+                direction = -1;
+            }
+            descendreLeft = true;
+        } else if (toutesLesDivs[alienInvaders[i]].getAttribute("data-left") === "true") {
+            if (descendreLeft) {
+                direction = 20;
+                setTimeout(() => { descendreLeft = false; }, 50);
+            } else if (descendreLeft === false) {
+                direction = 1;
+            }
+            descendreRight = true;
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        toutesLesDivs[alienInvaders[i]].classList.remove("alien");
+    }
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction;
+    }
+    for (let i = 0; i < alienInvaders.length; i++) {
+        toutesLesDivs[alienInvaders[i]].classList.add("alien");
+    }
+}
+invaderId = setInterval(bougerLesAliens, 500);
