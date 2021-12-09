220cc06ef9e9a4fedf3817b0eef868521ca7529f
Naomi Mango <naomi.mango@gmail.com>
2021-12-09 13:46:35 +0100

    [MAJ] P15 Space-Invaders - HTML, CSS & création de la grille

--- 15_Space Invaders/app.js
+++ 15_Space Invaders/app.js
@@ -1,1 +1,26 @@
+const container = document.querySelector(".grille");
+let toutesLesDiv;
+let alienInvaders = [];
 
+function creationGrilleEtAliens() {
+    let indexAttr = 0;
+
+    for (let i = 0; i < 240; i++) {
+        if (indexAttr === 0) {
+            const bloc = document.createElement("div");
+            bloc.setAttribute("data-left", "true");
+            container.appendChild(bloc);
+            indexAttr++;
+        } else if (indexAttr === 19) {
+            const bloc = document.createElement("div");
+            bloc.setAttribute("data-right", "true");
+            container.appendChild(bloc);
+            indexAttr = 0;
+        } else {
+            const bloc = document.createElement("div");
+            container.appendChild(bloc);
+            indexAttr++;
+        }
+    }
+}
+creationGrilleEtAliens();
