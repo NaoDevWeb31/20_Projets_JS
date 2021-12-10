2a90346b95d18427e5f03fe7b4d4407c41173c35
Naomi Mango <naomi.mango@gmail.com>
2021-12-10 13:21:27 +0100

    [MAJ] P17 Casse-briques - Animer le jeu

--- 17_CasseBriques/app.js
+++ 17_CasseBriques/app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const affichageScore = document.querySelector(".score");
 
 const rayonBalle = 10,
     barreHeight = 10,
@@ -11,7 +12,10 @@ const rayonBalle = 10,
 
 let x = canvas.width / 2,
     y = canvas.height - 30,
-    barreX = (canvas.width - barreWidth) / 2;
+    barreX = (canvas.width - barreWidth) / 2,
+    fin = false,
+    vitesseX = 5,
+    vitesseY = -5;
 
 function dessineBalle() {
     ctx.beginPath();
@@ -61,4 +65,49 @@ function dessineBriques() {
         }
     }
 }
-dessineBriques();
+// dessineBriques();
+
+function dessine() {
+    if (fin === false) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dessineBriques();
+        dessineBalle();
+        dessineBarre();
+
+        if (
+            x + vitesseX > canvas.width - rayonBalle ||
+            x + vitesseX < rayonBalle
+        ) {
+            vitesseX = -vitesseX;
+        }
+
+        if (y + vitesseY < rayonBalle) {
+            vitesseY = -vitesseY;
+        }
+
+        if (y + vitesseY > canvas.height - rayonBalle) {
+            // un intervalle => 0 - 75
+            if (x > barreX && x < barreX + barreX) {
+                vitesseX = vitesseX + 0.1;
+                vitesseY = vitesseY + 0.1;
+                vitesseY = -vitesseY;
+            } else {
+                fin = true;
+                affichageScore.innerHTML = `<span style="color: red;">🪦 PERDU 🪦</span> <br> Clique sur le casse-briques pour recommencer 🕹`;
+            }
+        }
+
+        x += vitesseX;
+        y += vitesseY;
+        requestAnimationFrame(dessine);
+    }
+}
+dessine();
+
+// Recommencer
+canvas.addEventListener("click", () => {
+    if (fin === true) {
+        fin = false;
+        document.location.reload();
+    }
+});
