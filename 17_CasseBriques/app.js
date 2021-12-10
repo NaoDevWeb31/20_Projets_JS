db9419db76337f0c64a9f1c5c0eb85c83ba858bd
Naomi Mango <naomi.mango@gmail.com>
2021-12-10 13:49:51 +0100

    [MAJ] P17 Casse-briques - Gérer la détection de collision

--- 17_CasseBriques/app.js
+++ 17_CasseBriques/app.js
@@ -14,8 +14,11 @@ let x = canvas.width / 2,
     y = canvas.height - 30,
     barreX = (canvas.width - barreWidth) / 2,
     fin = false,
-    vitesseX = 5,
-    vitesseY = -5;
+    vitesseX = 3,
+    vitesseY = -3,
+    score = 0;
+
+
 
 function dessineBalle() {
     ctx.beginPath();
@@ -24,7 +27,7 @@ function dessineBalle() {
     ctx.fill();
     ctx.closePath();
 }
-dessineBalle();
+// dessineBalle();
 
 function dessineBarre() {
     ctx.beginPath();
@@ -33,7 +36,9 @@ function dessineBarre() {
     ctx.fill();
     ctx.closePath();
 }
-dessineBarre();
+// dessineBarre();
+
+
 
 // Tableau avec toutes les briques
 const briques = [];
@@ -67,12 +72,15 @@ function dessineBriques() {
 }
 // dessineBriques();
 
+
+
 function dessine() {
     if (fin === false) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         dessineBriques();
         dessineBalle();
         dessineBarre();
+        collisionDetection();
 
         if (
             x + vitesseX > canvas.width - rayonBalle ||
@@ -87,7 +95,7 @@ function dessine() {
 
         if (y + vitesseY > canvas.height - rayonBalle) {
             // un intervalle => 0 - 75
-            if (x > barreX && x < barreX + barreX) {
+            if (x > barreX && x < barreX + barreWidth) {
                 vitesseX = vitesseX + 0.1;
                 vitesseY = vitesseY + 0.1;
                 vitesseY = -vitesseY;
@@ -103,6 +111,54 @@ function dessine() {
     }
 }
 dessine();
+
+
+
+// Collision de la balle contre les briques
+function collisionDetection() {
+    for (let i = 0; i < nbRow; i++) {
+        for (let j = 0; j < nbCol; j++) {
+            let b = briques[i][j];
+            if (b.statut === 1) {
+                if (
+                    x > b.x &&
+                    x < b.x + largeurBrique &&
+                    y > b.y &&
+                    y < b.y + hauteurBrique
+                ) {
+                    vitesseY = -vitesseY;
+                    b.statut = 0;
+
+                    score++;
+                    affichageScore.innerHTML = `Score : ${score}`;
+
+                    if (score === nbCol * nbRow) {
+                        affichageScore.innerHTML = `<span style="color: green;">🎉 Bravo 🎉</span> <br> Clique sur le casse-briques pour recommencer 🕹`;
+                        fin = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+// Mouvement de la barre
+document.addEventListener("mousemove", mouvementSouris);
+function mouvementSouris(e) {
+    let posXBarreCanvas = e.clientX - canvas.offsetLeft;
+    // e.clienX => de la gauche jusqu'à la souris
+    // canvas.offsetLeft => décalage par rapport à la gauche
+    
+    // console.log(posXBarreCanvas);
+
+    if (posXBarreCanvas > 35 && posXBarreCanvas < canvas.width - 35) {
+        barreX = posXBarreCanvas - barreWidth / 2;
+    }
+}
+
+
 
 // Recommencer
 canvas.addEventListener("click", () => {
