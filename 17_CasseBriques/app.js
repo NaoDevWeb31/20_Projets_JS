689c6a2ab14cc4af19f8b538f21b385db62b3300
Naomi Mango <naomi.mango@gmail.com>
2021-12-10 12:43:34 +0100

    [MAJ] P17 Casse-briques - Dessiner les briques

--- 17_CasseBriques/app.js
+++ 17_CasseBriques/app.js
@@ -3,7 +3,11 @@ const ctx = canvas.getContext("2d");
 
 const rayonBalle = 10,
     barreHeight = 10,
-    barreWidth = 75;
+    barreWidth = 75,
+    nbCol = 8,
+    nbRow = 5,
+    largeurBrique = 75,
+    hauteurBrique = 20;
 
 let x = canvas.width / 2,
     y = canvas.height - 30,
@@ -26,3 +30,35 @@ function dessineBarre() {
     ctx.closePath();
 }
 dessineBarre();
+
+// Tableau avec toutes les briques
+const briques = [];
+for (let i = 0; i < nbRow; i++) {
+    briques[i] = [];
+
+    for (let j = 0; j < nbCol; j++) {
+        briques[i][j] = { x: 0, y: 0, statut: 1 };
+    }
+}
+// console.log(briques);
+
+function dessineBriques() {
+    for (let i = 0; i < nbRow; i++) {
+        for (let j = 0; j < nbCol; j++) {
+            if (briques[i][j].statut === 1) {
+                let briqueX = j * (largeurBrique + 10) + 35;
+                let briqueY = i * (hauteurBrique + 10) + 30;
+
+                briques[i][j].x = briqueX;
+                briques[i][j].y = briqueY;
+
+                ctx.beginPath();
+                ctx.rect(briqueX, briqueY, largeurBrique, hauteurBrique);
+                ctx.fillStyle = "#333";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+dessineBriques();
