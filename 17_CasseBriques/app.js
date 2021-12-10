97ecc621fb9f035f85117c58260ecb2100875005
Naomi Mango <naomi.mango@gmail.com>
2021-12-10 12:07:02 +0100

    [MAJ] P17 Casse-briques - HTML, CSS & Création de la balle et la barre

--- 17_CasseBriques/app.js
+++ 17_CasseBriques/app.js
@@ -1,1 +1,28 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
+const rayonBalle = 10,
+    barreHeight = 10,
+    barreWidth = 75;
+
+let x = canvas.width / 2,
+    y = canvas.height - 30,
+    barreX = (canvas.width - barreWidth) / 2;
+
+function dessineBalle() {
+    ctx.beginPath();
+    ctx.arc(x, y, rayonBalle, 0, Math.PI * 2);
+    ctx.fillStyle = "#333";
+    ctx.fill();
+    ctx.closePath();
+}
+dessineBalle();
+
+function dessineBarre() {
+    ctx.beginPath();
+    ctx.rect(barreX, canvas.height - barreHeight - 2, barreWidth, barreHeight);
+    ctx.fillStyle = "#333";
+    ctx.fill();
+    ctx.closePath();
+}
+dessineBarre();
